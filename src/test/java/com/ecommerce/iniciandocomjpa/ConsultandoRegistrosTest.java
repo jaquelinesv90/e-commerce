98bfdc415b9b2f4f271802d918886decf1bb09da
Jaqueline <jaquelinesv90@gmail.com>
2021-07-26 14:03:24 -0300

    criada primeira classe de teste com Junit

---
+++ src/test/java/com/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -0,0 +1,43 @@
+package com.ecommerce.iniciandocomjpa;
+
+import com.ecommerce.model.Produto;
+import org.junit.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConsultandoRegistrosTest {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeClass
+    public static void setUpBeforeClass(){
+        entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass(){
+        entityManagerFactory.close();
+    }
+
+    @Before
+    public void setUp(){
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @After
+    public void tearDown(){
+        entityManager.close();
+    }
+
+    @Test
+    public void buscarPorIdentificador(){
+        Produto produto = entityManager.find(Produto.class, 1);
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("kindle", produto.getNome());
+    }
+}
