9a07db7dd33888a2bea0c4aaba5d19c4f65f99eb
jaquelinesv90 <jaquelinesv90@gmail.com>
2022-07-06 13:45:26 -0700

    adicionada classe para exemplo de controle de transação

---
+++ src/test/java/com/ecommerce/conhecendoentitymanager/GerenciamentoTransacoesTest.java
@@ -0,0 +1,12 @@
+package com.ecommerce.conhecendoentitymanager;
+
+import com.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class GerenciamentoTransacoesTest extends EntityManagerTest {
+
+    @Test
+    public void abrirFecharCancelarTransacao(){
+
+    }
+}
